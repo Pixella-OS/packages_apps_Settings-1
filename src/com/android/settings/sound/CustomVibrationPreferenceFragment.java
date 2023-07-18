Flamingo-OS/packages_apps_Settings

[SQUASH] Settings: Add vibration patterns from OOS with custom vibration patterns[3/3]
a rewrite of: https://github.com/AOSPA/android_packages_apps_Settings/commit/c4560cafae3f97f09c372c5289869b35c90d3b71

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

This is the commit message #2:

Settings: Allow choosing a custom vibration pattern [3/3]

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

This is the commit message #3:

Settings: Preview ringtone vibration pattern

This is the commit message #4:

Settings: Follow new ringtone vibration patterns

For preview

See: https://github.com/yaap/packages_services_Telecomm/commit/7b3ab1300210adc23c1441953c80a4778ca8e312

This is the commit message #5:

Settings: Move custom vib pattern to its own preference

Also improve the code while at it

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

This is the commit message #6:

Settings: move vibration pattern prefs to new vibration settings screen

* also fix dependency of custom vibration pref and use BasePreferenceController for vibration pref

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

This is the commit message #7:

Settings: improve custom vibration preference

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>

src/com/android/settings/sound/CustomVibrationPreferenceFragment.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2020 Yet Another AOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.sound;
+
+import android.content.Context;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Settings for custom ringtone vibration pattern
+ */
+public class CustomVibrationPreferenceFragment extends DashboardFragment {
+
+    private static final String TAG = "CustomVibrationPreferenceFragment";
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.YASP;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.custom_vibration_pattern;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new CustomVibrationPreferenceController(context));
+        return controllers;
+    }
+}

Pixella-OS/packages_apps_Settings-1

Change-Id: I44663da3ab378786eb306e96a51be39a3e733997
Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/android/settings/sound/CustomVibrationPreferenceFragment.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2020 Yet Another AOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.sound;
+
+import android.content.Context;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Settings for custom ringtone vibration pattern
+ */
+public class CustomVibrationPreferenceFragment extends DashboardFragment {
+
+    private static final String TAG = "CustomVibrationPreferenceFragment";
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CUSTOM_SETTINGS;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.custom_vibration_pattern;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new CustomVibrationPreferenceController(context));
+        return controllers;
+    }
+}